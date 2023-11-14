b3dde3c1261054ea563669169a251f633d4e91dd
HabeebEasy <144483222+HabeebEasy@users.noreply.github.com>
2023-11-14 20:08:00 +0100

    Update print_percent.c 1

--- print_percent.c
+++ print_percent.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
  * print_percent - Prints a percent sign
- * @types: Lista of arguments
+ * @types: various arguments
  * @buffer: Buffer array to handle print
  * @flags:  Calculates active flags
  * @width: get width.
