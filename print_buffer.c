c46cf31c230ebf1dd6aad92f41a110f9b4d9d3a1
peocloo <philemoneyramocloo@gmail.com>
2023-11-14 14:09:05 -0800

    custom printf update

--- print_buffer.c
+++ print_buffer.c
@@ -1,55 +1,56 @@
-#include "main.h"
+include "main.h"
 #include <stdarg.h>
 
 void print_buffer(char buffer[], int *buff_ind);
 /**
  * _printf - custom printf function
  * @format: arugment for formats to be printed
- * 
+ *
  * Return: integer
 */
 int _printf(const char *format, ...)
 {
-    int i, j = 0, k = 0;
-    int flags, width, precision, size, buff_ind = 0;
-    va_list args;
-    char buffer[BUFF_SIZE];
+	int i, j = 0, k = 0;
+	int flags, width, precision, size, buff_ind = 0;
+	va_list args;
+	char buffer[BUFF_SIZE];
 
-    if (format == NULL)
-        return (-1);
+	if (format == NULL)
+		return (-1);
 
-    va_start(args, format);
+	va_start(args, format);
 
-    for (i = 0; format && format[i] != '\0'; i++)
-    {
-        if (format[i] != '%')
-        {
-            buffer[buff_ind++] = format[i];
-            if (buff_ind == BUFF_SIZE)
-                print_buffer(buffer, &buff_ind);
-            k++;
-        }
-        else
-        {
-            print_buffer(buffer, &buff_ind);
-            flags = get_flags(format, &i);
-            width = get_width(format, &i, args);
-            precision = get_precision(format, &i, args);
-            size = get_size(format, &i);
-            ++i;
-            j = handle_print(format, &i, args, buffer,
-                             flags, width, precision, size);
-            if (j == -1)
-                return (-1);
-            k += j;
-        }
-    }
+	for (i = 0; format && format[i] != '\0'; i++)
+	{
+		if (format[i] != '%')
+		{
+			buffer[buff_ind++] = format[i];
+			if (buff_ind == BUFF_SIZE)
+			print_buffer(buffer, &buff_ind);
+			k++;
+		}
+		else
+		{
+			print_buffer(buffer, &buff_ind);
+			flags = get_flags(format, &i);
+			width = get_width(format, &i, args);
+			precision = get_precision(format, &i, args);
+			size = get_size(format, &i);
+			++i;
+			j = handle_print(format, &i, args, buffer,
+				flags, width, precision, size);
+			if (j == -1)
+				return (-1);
 
-    print_buffer(buffer, &buff_ind);
+			k += j;
+		}
+	}
 
-    va_end(args);
+	print_buffer(buffer, &buff_ind);
 
-    return (k);
+	va_end(args);
+
+	return (k);
 }
 
 /**
@@ -59,8 +60,8 @@ int _printf(const char *format, ...)
 */
 void print_buffer(char buffer[], int *buff_ind)
 {
-    if (*buff_ind > 0)
-        write(1, &buffer[0], *buff_ind);
+	if (*buff_ind > 0)
+		write(1, &buffer[0], *buff_ind);
 
-    *buff_ind = 0;
+	*buff_ind = 0;
 }
