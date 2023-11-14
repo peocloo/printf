cc457b9ed6eeb5fb34c5afa9e9ae46435de2875f
HabeebEasy <144483222+HabeebEasy@users.noreply.github.com>
2023-11-14 20:22:18 +0100

    Update print_pointer.c  1

--- print_pointer.c
+++ print_pointer.c
@@ -2,7 +2,7 @@
 
 /**
  * print_pointer - Prints the value of a pointer variable
- * @types: List a of arguments
+ * @types: various arguments
  * @buffer: Buffer array to handle print
  * @flags:  Calculates active flags
  * @width: get width
